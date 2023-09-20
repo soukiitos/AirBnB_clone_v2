29fc3903c94261bb877135299929312565d91a71
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-09-20 02:45:56 +0100

    Task 8

--- models/user.py
+++ models/user.py
@@ -12,6 +12,6 @@ class User(BaseModel, Base):
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
     places = relationship(
-            'Place', backref='user',
-            cascade='all, delete-orphan'
+            "Place", backref="user",
+            cascade="all, delete-orphan"
             )
