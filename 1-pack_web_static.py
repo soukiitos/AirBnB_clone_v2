decd7e95d796ae41c3c2a04fe8aadbfccf5ed892
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-05 18:32:46 +0100

    test

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -11,20 +11,22 @@ import os
 @runs_once
 def do_pack():
     '''Define do_pack()'''
-    if not os.path.exists("versions"):
+    if not os.path.isdir("versions"):
         os.mkdir("versions")
-    timest = datetime.now()
-    res = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-            timest.year,
-            timest.month,
-            timest.day,
-            timest.hour,
-            timest.minute,
-            timest.second
-            }
+    d_time = datetime.now()
+    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+            d_time.year,
+            d_time.month,
+            d_time.day,
+            d_time.hour,
+            d_time.minute,
+            d_time.second
+            )
     try:
-        c.local("tar -czvf versions/{} web_static".format(res))
-        res_path = os.path.join("versions", res)
-        return res_path
+        print("Packing web_static to {}".format(output))
+        local("tar -cvzf {} web_static".format(output))
+        size = os.stat(output).st_size
+        print("web_static packed: {} -> {} Bytes".format(output, size))
     except Exception:
-        return None
+        output = None
+    return output
