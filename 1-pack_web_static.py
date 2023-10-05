8feb9d24521aece38a18d70b200a8e9a01d4e8ff
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-05 18:22:24 +0100

    Task 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Generate a .tgz archive from the contents of the web_static folder
+of our AirBnB Clone repo, using the function do_pack
+"""
+from fabric import task
+from datetime import datetime
+import os
+
+
+def do_pack():
+    '''Define do_pack()'''
+    if not os.path.exists("versions"):
+        os.mkdir("versions")
+    timest = datetime.now()
+    res = "versions/web_static_{}{}{}{}{}{}.tgz".format(
+            timest.year,
+            timest.month,
+            timest.day,
+            timest.hour,
+            timest.minute,
+            timest.second
+            }
+    try:
+        c.local("tar -czvf versions/{} web_static".format(res))
+        res_path = os.path.join("versions", res)
+        return res_path
+    except Exception:
+        return None
