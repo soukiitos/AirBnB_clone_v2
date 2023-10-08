8fdb4d64a7df5ec8d59067658e1a96d7be4515e6
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-08 16:44:34 +0100

    Task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,5 +25,26 @@ def do_pack():
 
 def do_deploy(archive_path):
     '''Define do_deploy()'''
-    if os.path.exists(archive_path):
-        archived_file = archive_path[9:]
+    try:
+        if not os.path.exists(archive_path):
+            return False
+        f_with_ext = os.path.basename(archive_path)
+        f_with_no_ext, ext = os.path.splitext(f_with_ext)
+        d_path = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(d_path, f_with_no_ext))
+        run("mkdir -p {}{}/".format(d_path, f_with_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(
+            f_with_ext, d_path, f_with_no_ext
+            ))
+        run("rm /tmp/{}".format(f_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(d_path, f_with_no_ext))
+        run("rm -rf {}{}/web_static".format(d_path,  f_with_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(
+            d_path, f_with_no_ext
+            ))
+        print("New version deployed!")
+        return True
+    except Exception:
+        return False
