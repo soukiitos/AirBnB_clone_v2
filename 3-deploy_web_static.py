39a8f2482f3afefec58c23155540758658e7d6e7
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-08 20:43:28 +0000

    Task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -3,42 +3,46 @@
 creates and distributes an archive to your web servers,
 using the function deploy
 """
-from fabric.api import env, local, put, run
+from fabric.api import task, env, local, put, run, runs_once
 from datetime import datetime
-from os.path import exists, isdir
+import os
 
 env.hosts = ['100.26.249.88', '100.26.50.67']
 
 
 def do_pack():
     '''Define do pack'''
-    try:
         d = datetime.now.strftime("%Y%m%d%H%M%S")
-        if isdir("versions") is False:
-            local("mkdir versions")
-        f_name = "versions/web_static_{}.tgz".format(d)
-        local("tar -cvzf {} web_static_{}".format(f_name))
-        return f_name
-    except Exception:
+mkdir = "mkdir -p versions"
+path = "versions/web_static_{}.tgz".format(d)
+print("Packing web_static to {}".format(path))
+if local("{} && tar -cvzf {} web_static".format(mkdir, path)).succeeded:
+return path
         return None
 
 
 def do_deploy(archive_path):
     '''Define do deploy()'''
-    if exists(archive_path) is False:
-        return False
-    try:
-        n_file = archive_path.split("/")[-1]
-        n_ext = n_file.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, n_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(n_file, path, n_ext))
-        run('rm /tmp/{}'.format(n_file))
-        run('mv {0}{1}/web_static/* {0}{1}'.format(path, n_ext))
-        run('rm -rf {}{}/web_static'.format(path, n_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, n_ext))
+try:
+if not os.path.exists(archive_path):
+            return False
+        f_with_ext = os.path.basename(archive_path)
+        f_with_no_ext, ext = os.path.splitext(f_with_ext)
+        d_path = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(d_path, f_with_no_ext))
+        run("mkdir -p {}{}/".format(d_path, f_with_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(
+            f_with_ext, d_path, f_with_no_ext
+            ))
+        run("rm /tmp/{}".format(f_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(d_path, f_with_no_ext))
+        run("rm -rf {}{}/web_static".format(d_path,  f_with_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(
+            d_path, f_with_no_ext
+            ))
+        print("New version deployed!")
         return True
     except Exception:
         return False
@@ -46,7 +50,7 @@ def do_deploy(archive_path):
 
 def deploy():
     '''Define deploy()'''
-    archive_path = do_pack()
-    if archive_path is None:
+    path = do_pack()
+    if path is None:
         return False
-    return do_deploy(archive_path)
+    return do_deploy(path)
