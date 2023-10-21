e869dffe5eb6b62866fe6742c1866ae96f3de3be
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-21 22:09:47 +0100

    Start a Flask web application

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+'''Start a Flask web application'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_hbnb():
+    '''Define hello hbnb'''
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    '''Define hbnb'''
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    '''Define c is fun'''
+    return 'C ' + text.replace('_', ' ')
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    '''Define python with text'''
+    return 'Python ' + text.replace('_', ' ')
+
+
+@app.route('/number/<int:n>')
+def number(n=None):
+    '''Define number'''
+    return str(n) + ' is a number'
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
