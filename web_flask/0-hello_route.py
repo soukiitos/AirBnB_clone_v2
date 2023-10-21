f2b3d2d27cda16a8051e96638c7cbf078daffa56
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-21 02:23:52 +0100

    README file and Task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Start a Flask web application'''
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_flask():
+    '''Define a Hello Flask'''
+    return 'Hello Flask!'
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
