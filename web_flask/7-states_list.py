c71a91154392115196c4d41da8c0d3812e7252f7
Soukaina Lachheb <soukainalachheb93@gmail.com>
2023-10-22 19:57:43 +0100

    Task 8

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Start a Flask web application'''
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+app = Flask(__name__)
+
+
+@app.route('/states_list')
+def state_list():
+    '''Define state list'''
+    path = '7-states_list.html'
+    states = storage.all(State)
+    sorted_state = sorted(states.values(), key=lambda state: state.name)
+    return render_template(path, sorted_state=sorted_state)
+
+
+@app.teardown_appcontext
+def app_teardown(arg=None):
+    '''Define app teardown'''
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
